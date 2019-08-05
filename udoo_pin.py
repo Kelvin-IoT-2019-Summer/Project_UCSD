7617216a35afa9b8f716982153ef9f173f0c1070
Kelvin-IoT-2019-Summer <ffotturish@naver.com>
2019-08-05 05:30:23 +0000

    This is a new commit for what I originally planned to be amended

---
+++ udoo_pin.py
@@ -0,0 +1,30 @@
+def p_TMP():
+	return 0
+
+def p_NO2_WE():
+	return 1
+
+def p_NO2_AE():
+	return 2
+
+def p_O3_WE():
+	return 3
+
+def p_O3_AE():
+	return 4
+
+def p_CO_WE():
+	return 5
+
+def p_CO_AE():
+	return 6
+
+def p_SO2_WE():
+	return 7
+
+def p_SO2_AE():
+	return 8
+
+def p_PM25():
+	return 9
+
